b2fa45459c9167217c22f9c178a5246140bb4844
Matheus Bello <omanacleto@hotmail.com>
2024-05-09 16:50:57 -0300

    Better library imports

--- routes/libs.ts
+++ routes/libs.ts
@@ -1,16 +1,16 @@
 import { app } from "..";
 
-app.get("/jquery", async (req, res) => {
- const src = await Bun.file("./frontend/libs/jquery.min.js");
+app.get("/jquery.js", async (req, res) => {
+ const src = await Bun.file("./frontend/libs/jquery.min.js").text();
  res.setHeader("content-type", "application/javascript").status(200).send(src);
 });
 
-app.get("/tailwind", async (req, res) => {
- const src = await Bun.file("./frontend/libs/tailwind.min.js");
+app.get("/tailwind.js", async (req, res) => {
+ const src = await Bun.file("./frontend/libs/tailwind.min.js").text();
  res.setHeader("content-type", "application/javascript").status(200).send(src);
 });
 
 app.get("/nanoid", async (req, res) => {
- const src = await Bun.file("./frontend/libs/nanoid.min.js");
+ const src = await Bun.file("./frontend/libs/nanoid.min.js").text();
  res.setHeader("content-type", "application/javascript").status(200).send(src);
 });
