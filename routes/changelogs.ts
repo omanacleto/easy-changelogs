ae4aea60617441f3beb6f5ae978d8074efad10b9
Matheus Bello <omanacleto@hotmail.com>
2024-06-05 19:03:23 -0300

    fixing markdown parsing on date based changelogs

--- routes/changelogs.ts
+++ routes/changelogs.ts
@@ -186,7 +186,7 @@ app.get("/changelogs", async (req, res) => {
 
    logsByDate[date].forEach(({ type, message, id }) => {
     const nLog = log
-     .replaceAll("[MESSAGE]", message)
+     .replaceAll("[MESSAGE]", parseMd(message))
      .replaceAll("[REMOVEBUTTON]", isLoggedIn ? removeBtn : "")
      .replaceAll("[LOGID]", id.toString());
 
